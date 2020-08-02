2d7a456f12cf519b5a89267f74baa0befd920eb9
anonymous3141 <50539384+anonymous3141@users.noreply.github.com>
2020-08-02 16:56:09 +1000

    Add files via upload

---
+++ game/generators/tkgen.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N,K,SB;
+    cin >> N >> K >> SB;
+    if (SB == 1) {assert(K == 1);}
+    if (SB == 3) {assert(N <= 1000);}
+    if (SB == 4) {assert(K == N-1);}
+    vector <int> Vals;
+    for (int i=0; i<N; i++) {
+        Vals.push_back((rand()-RAND_MAX/2)%100000);
+    }
+    if (SB == 2) {
+        sort(Vals.begin(), Vals.end());
+    }
+    printf("%d %d\n",N,K);
+    for (int i=0; i<N; i++) {
+        printf("%d ",Vals[i]);
+    }
+}
